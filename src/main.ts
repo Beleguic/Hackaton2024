eeb8311789055dfe10c753b5cf7441276fe6b09e
Thibault Beleguic <thibault.beleguic2@gmail.com>
2024-04-26 06:24:53 +0200

    The floor on the Movie theater is now dynamic

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,35 @@ console.log("Script started successfully");
 
 WA.onInit()
     .then(async () => {
-        console.log("Scripting API ready");
+            console.log("Scripting API ready");
 
-        let sport = WA.room.hashParameters.sport;
-        sport = decodeURI(sport);
-        console.log(sport);
+            let sport = WA.room.hashParameters.sport;
+            sport = decodeURI(sport);
+            console.log(sport);
 
-        // Get an existing website object where 'my_website' is the name of the object (on any layer object of the map)
-        const website = await WA.room.website.get("video-player");
-        website.url = "video_youtube.html?video=" + sport;
-        website.visible = true;
+            if(sport === "Football"){
+                WA.room.showLayer("floors/foot");
+                WA.room.hideLayer("floors/floorblue");
+            }
+            else if(sport === "Formule 1") {
+                WA.room.showLayer("floors/formuleone");
+                WA.room.hideLayer("floors/floorblue");
+            }
+            else{
+                WA.room.showLayer("floors/floorblue");
+                WA.room.hideLayer("floors/foot");
+                WA.room.hideLayer("floors/formuleone");
+            }
 
-        const score = await WA.room.website.get("score-player");
-        score.url = "score.html?video=" + sport;
-        score.visible = true;
+            // Get an existing website object where 'my_website' is the name of the object (on any layer object of the map)
+            const website = await WA.room.website.get("video-player");
+            website.url = "video_youtube.html?video=" + sport;
+            website.visible = true;
+
+            const score = await WA.room.website.get("score-player");
+            score.url = "score.html?video=" + sport;
+            score.visible = true;
+
+
 
     });
