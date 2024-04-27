c36ee248750c86166197a93c9daae29f89fb6742
Thibault Beleguic <thibault.beleguic2@gmail.com>
2024-04-27 22:16:57 +0200

    Fix CORS

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     },
     plugins: [...getMapsOptimizers(maps, optimizerOptions)],
     server: {
-        host: "localhost",
+        host: "github.io",
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
